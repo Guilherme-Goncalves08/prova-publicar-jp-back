ec63936d1aaa7de46176be55f2837648a6db41ab
Guilherme <gui.goncalves.joi@gmail.com>
2025-11-28 20:08:45 -0300

    feat init

---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+function Home() {
+    return (
+        <div className="home">
+            Outras informações e um
+            {/* <Link to='/rick-and-morty'>
+                <button>
+                    Navegar para API
+                </button>
+            </Link> */}
+        </div>
+    )
+}
+
+export default Home
