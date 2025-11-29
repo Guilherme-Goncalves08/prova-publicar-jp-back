5c594a7e852f15f4cfd0860d6254d31bf8d3e8e0
Guilherme <gui.goncalves.joi@gmail.com>
2025-11-28 22:10:24 -0300

    feat db

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -8,10 +8,10 @@ class Database {
     init() {
         // .env - dotenv
         this.db = new Sequelize({
-            database: 'exemplo',
-            host: 'localhost',
-            username: 'root',
-            password: '',
+            database: 'exemplo_vo9t',
+            host: 'dpg-d4l3q1u3jp1c7395m36g-a',
+            username: 'exemplo_vo9t_user',
+            password: 'riBoGTJJVJO8L6mt9nTG1NTr7AKI24NI',
             dialect: 'mysql'
         })
     }
